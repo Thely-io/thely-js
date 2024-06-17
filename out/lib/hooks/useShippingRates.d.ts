1d2cfd7b37dde20d8ebc36834ac0e13efee885b5
Aj Wazzan <x_1337@outlook.com>
2024-06-17 14:48:33 -0700

    fixing types

--- out/lib/hooks/useShippingRates.d.ts
+++
@@ -1,7 +0,0 @@
-import { type ShipmentData } from '../../types.ts';
-export declare const useShippingRates: (parcel: string, address: string) => {
-    shipment: ShipmentData;
-    error: Record<string, any> | null;
-    loading: boolean;
-};
-//# sourceMappingURL=useShippingRates.d.ts.map
